d6db20a9fd934c224b632054b45eeecdf09e6994
bigAlbegovProffesor <roomsmassage451@gmail.com>
2025-10-29 21:18:44 +0300

    update by marlen dated-29/10/25 v.2

--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,24 @@
 package storage
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"project2/domain"
+)
+
 type Storable interface {
 	Save() error
 	Load() error
+}
+
+func Save(filename string, book []*domain.Book) {
+	fmt.Println("This goes into the text file")
+	data := []byte("first line\n second line")
+	err := os.WriteFile("log.txt", data, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File log.txt created")
+
 }
